b7baf0b4c7eafd2026b35881ee35fc8782c0a0d4
poorva <poorva.ramani@gmail.com>
2019-10-18 08:03:39 -0500

    saving progress in timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,23 @@
 var hourDiv = $(".hour-div");
-timeArr=["10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
+timeArr=["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
 $(document).ready(function(){
+
+$("#date-time").text(moment().format('dddd,  MMMM Do YYYY'));
+            
  for(var i=0;i<8;i++){
-    $(".time").text(timeArr[i]);
     hourDiv.clone().appendTo(".container");
  }
+        $.each(timeArr, function(index,value){
+            console.log(index);
+            console.log(value);
+            $(".time").text(value);
+        });
+
+        // $.each($('.productDescription'), function (index, value) {
+        //     console.log(index + ':' + $(value).text());
+        //     });
+
+        //     $('a').each(function (index, value){
+        //         console.log($(this).attr('href'));
+        //         });
 });
