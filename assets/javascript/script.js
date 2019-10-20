5bb4e807485cb44a7b73d1c86aeff9553e018617
poorva <poorva.ramani@gmail.com>
2019-10-20 14:12:53 -0500

    updating before/after time logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,11 @@
+var closeBtn = document.querySelector("#close-btn");
+var closeButton = document.querySelector(".close-button");
+var modalText = document.querySelector("#modal-text");
+var modalSubtext = document.querySelector("#modal-subtext");
+var modal = document.querySelector(".modal");
+
 var hourDiv = $(".hour-div");
-var hour = ["9 AM", "10 AM", "11 AM", "12 AM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var hour = ["09 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 $(document).ready(function () {
   $("#date-time").text(moment().format('dddd,  MMMM Do YYYY'));
   //creating 9 divs
@@ -7,29 +13,16 @@ $(document).ready(function () {
     hourDiv.clone().appendTo(".container");
   }
 
+  function toggleModal() {
+    modal.classList.toggle("show-modal");
+  }
+
   var planner = $(".planner");
   var time = $(".time");
 
   // displaying time in divs  
   $.each(time, function (i) {
-    $(this).attr("data-letter", i);
     $(this).text(hour[i]);
-  });
-
-  $.each(planner, function (i) {
-    if ($(".time").eq(i).text() === moment().format('h A')) {
-      console.log("equal" + $(".time").eq(i).text())
-      $(this).css("background-color", "#FE2020")
-    }
-    else if ($(".time").eq(i).text() < moment().format('h A')) {
-      console.log("before" + $(".time").eq(i).text())
-
-      $(this).css("background-color", "#0E86D4")
-    }
-    else if ($(".time").eq(i).text() > moment().format('h A')) {
-      console.log("after" + $(".time").eq(i).text())
-      $(this).css("background-color", "#96bf21")
-    }
   });
 
   var plannerData =
@@ -39,7 +32,7 @@ $(document).ready(function () {
         { time: "9 AM", eventDetail: "" },
         { time: "10 AM", eventDetail: "" },
         { time: "11 AM", eventDetail: "" },
-        { time: "12 AM", eventDetail: "" },
+        { time: "12 PM", eventDetail: "" },
         { time: "1 PM", eventDetail: "" },
         { time: "2 PM", eventDetail: "" },
         { time: "3 PM", eventDetail: "" },
@@ -48,7 +41,19 @@ $(document).ready(function () {
       ]
   };
   
-  var restoredData = JSON.parse(localStorage.getItem('plannerData'));
+  $.each(planner, function (i) {
+    if ($(".time").eq(i).text() === moment().format('h A')) {
+      $(this).css("background-color", "#FE2020");
+    }
+    else if ($(".time").eq(i).text() > moment().format('h A')) {
+      $(this).css("background-color", "#96bf21");
+    }
+    else if ($(".time").eq(i).text() < moment().format('h A')) {
+      $(this).css("background-color", "#ccc");
+
+    }
+  });
+   var restoredData = JSON.parse(localStorage.getItem('plannerData'));
 
   function renderEvents() {
      restoredData = JSON.parse(localStorage.getItem('plannerData'));
@@ -62,15 +67,21 @@ $(document).ready(function () {
     $(this).on("click", function () {
       var restoredData = JSON.parse(localStorage.getItem('plannerData'));
       restoredData.obj[i].eventDetail = $(".planner").eq(i).val();
-      console.log(restoredData)
+      console.log(restoredData);
       localStorage.setItem('plannerData', JSON.stringify(restoredData));
     });
   });
+ $(".planner").each(function (i) {
+  $(this).on("click", function () {
+    modalText.style.color = "green";
+    modalText.textContent = "✨ Correct Answer !!  ✨";
+    modalSubtext.textContent = "You get 15 more seconds in your scoretime";
+    this.onclick = toggleModal();
+    closeButton.addEventListener("click", toggleModal);
+    closeBtn.addEventListener("click", toggleModal);
+  });
+});
 
   renderEvents();
-
-  // $.each(planner, function (i) {
-  //     $(this).text(plannerData)
-  // });
-
 });
+
