bb9cc36957bc54706b5b211adddea85ccdc11e4c
poorva <poorva.ramani@gmail.com>
2019-10-21 08:52:10 -0500

    fixing local storage issue and removing modal code to new repository.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,3 @@
-var closeBtn = document.querySelector("#close-btn");
-var closeButton = document.querySelector(".close-button");
-var modalText = document.querySelector("#modal-text");
-var modalSubtext = document.querySelector("#modal-subtext");
-var modal = document.querySelector(".modal");
-
 var hourDiv = $(".hour-div");
 var hour = ["09 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 $(document).ready(function () {
@@ -50,37 +44,25 @@ $(document).ready(function () {
     }
     else if ($(".time").eq(i).text() < moment().format('h A')) {
       $(this).css("background-color", "#ccc");
-
     }
   });
-   var restoredData = JSON.parse(localStorage.getItem('plannerData'));
 
   function renderEvents() {
-     restoredData = JSON.parse(localStorage.getItem('plannerData'));
-     localStorage.setItem('plannerData', JSON.stringify(plannerData));
+    restoredData = JSON.parse(localStorage.getItem('plannerData'));
+    console.log()
     for (var j = 0; j < restoredData.obj.length; j++) {
       $(".planner").eq(j).text(restoredData.obj[j].eventDetail);
     }
+    localStorage.setItem('plannerData', JSON.stringify(restoredData));
   }
 
   $(".save").each(function (i) {
     $(this).on("click", function () {
       var restoredData = JSON.parse(localStorage.getItem('plannerData'));
       restoredData.obj[i].eventDetail = $(".planner").eq(i).val();
-      console.log(restoredData);
       localStorage.setItem('plannerData', JSON.stringify(restoredData));
     });
   });
- $(".planner").each(function (i) {
-  $(this).on("click", function () {
-    modalText.style.color = "green";
-    modalText.textContent = "✨ Correct Answer !!  ✨";
-    modalSubtext.textContent = "You get 15 more seconds in your scoretime";
-    this.onclick = toggleModal();
-    closeButton.addEventListener("click", toggleModal);
-    closeBtn.addEventListener("click", toggleModal);
-  });
-});
 
   renderEvents();
 });
